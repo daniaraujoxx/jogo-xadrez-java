5517822b94474e219d44e65f5378dc56aef9353d
daniaraujoxx <danyeladearaujo@hotmail.com>
2020-09-22 15:48:43 -0300

    pacote chess

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,16 +1,15 @@
 package boardgame;
 
 public class Position {
-	
+
 	private int row;
 	private int column;
-	
+
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
 
-	
 	public int getRow() {
 		return row;
 	}
@@ -26,10 +25,9 @@ public class Position {
 	public void setColumn(int column) {
 		this.column = column;
 	}
-	
+
 	@Override
 	public String toString() {
-	return row + ", " + column;
-	
+		return row + ", " + column;
 	}
 }
