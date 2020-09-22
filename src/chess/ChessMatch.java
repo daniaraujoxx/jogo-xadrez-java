7f2c68a7813c02c2c8a04a09ffebda023160f63f
daniaraujoxx <danyeladearaujo@hotmail.com>
2020-09-22 17:47:06 -0300

    chessException and chessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,10 +26,17 @@ public class ChessMatch {
 		}
 		return mat;
 	}
+	
+	//esse metodo recebe as coordenadas do xadrez
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
 	//metodo responsavel por iniciar a partida de xadrez, colocando as peças no tabuleiro
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.BLACK));
+
 		
 		
 	}
